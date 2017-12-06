820b082331fce2901a485e9b411a9692f88453a3
eaajones <33253637+eaajones@users.noreply.github.com>
2017-12-06 12:58:36 +0100

    external javascript file added

--- index.php
+++ index.php
@@ -3,13 +3,27 @@
 <head>
 <meta charset="utf-8">
 <title>Basement</title>
-<link href="css/stylesheet.css" rel="stylesheet" type="text/css">
+<link href="css/stylesheet.css" rel="stylesheet" type="text/css">	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="Js/javascript.js"></script>
+	 <script>
+    $(document).ready(function() {
+        $("#test").click(function() {
+            $('#hello').hide('slide', {direction: 'left'}, 1000);
+        });
+    });
+   </script>
 </head>
 <body>
 <a class="button" class="btn-link" href="Game/Game.html">Start</a>
+<div id="contact">
+    Contact me!
+</div>
+<a href="#" id="toggle">Contact</a>
 	
 
 	
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>	
+	<div class="box"></div>
+
 </body>
 </html>
