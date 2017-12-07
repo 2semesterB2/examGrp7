f85f13fc02be417da4dc59cf177b3a649ea9297d
Hamzeh <hslipp@hotmail.com>
2017-12-07 13:10:37 +0100

    STAR rate

--- Rate us/rateus.php
+++ Rate us/rateus.php
@@ -5,6 +5,7 @@
 <title>Rate Us</title>
 <link href="stylesheet.css" rel="stylesheet" type="text/css">	
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 <script src="javascript.js"></script>
 </head>
 <body>
@@ -12,14 +13,20 @@
 <h1>  RATE US  </h1>
     
 	
-<div id="rating">
-  <span>★</span>
-  <span>★</span>
-  <span>★</span>
-  <span>★</span>
-  <span>★</span>
+<div class="stars">
+  <form action="">
+    <input class="star star-5" id="star-5" type="radio" name="star"/>
+    <label class="star star-5" for="star-5"></label>
+    <input class="star star-4" id="star-4" type="radio" name="star"/>
+    <label class="star star-4" for="star-4"></label>
+    <input class="star star-3" id="star-3" type="radio" name="star"/>
+    <label class="star star-3" for="star-3"></label>
+    <input class="star star-2" id="star-2" type="radio" name="star"/>
+    <label class="star star-2" for="star-2"></label>
+    <input class="star star-1" id="star-1" type="radio" name="star"/>
+    <label class="star star-1" for="star-1"></label>
+  </form>
 </div>
-	
 	
 <a href="#"> </a><button class="button button2">Next</button>
     
