de14695bbf5b520fae6ed05e0ee9f002cf56004d
Hamzeh <hslipp@hotmail.com>
2017-12-07 12:48:31 +0100

    Star

--- Rate us/javascript.js
+++ Rate us/javascript.js
@@ -1,58 +1,52 @@
-// JavaScript Document
-$('.ratings_stars').hover(
-            // Handles the mouseover
-            function() {
-                $(this).prevAll().andSelf().addClass('ratings_over');
-                $(this).nextAll().removeClass('ratings_vote'); 
-            },
-            // Handles the mouseout
-            function() {
-                $(this).prevAll().andSelf().removeClass('ratings_over');
-                set_votes($(this).parent());
-            }
-        );
+/**
+ * Star rating class
+ * @constructor
+ */
+function StarRating() {
+  this.init();
+};
 
- $('.rate_widget').each(function(i) {
-            var widget = this;
-            var out_data = {
-                widget_id : $(widget).attr('id'),
-                fetch: 1
-            };
-            $.post(
-                'ratings.php',
-                out_data,
-                function(INFO) {
-                    $(widget).data( 'fsr', INFO );
-                    set_votes(widget);
-                },
-                'json'
-            );
-        });
-function set_votes(widget) {
-        
-            var avg = $(widget).data('fsr').whole_avg;
-            var votes = $(widget).data('fsr').number_votes;
-            var exact = $(widget).data('fsr').dec_avg;
-            
-            $(widget).find('.star_' + avg).prevAll().andSelf().addClass('ratings_vote');
-            $(widget).find('.star_' + avg).nextAll().removeClass('ratings_vote'); 
-            $(widget).find('.total_votes').text( votes + ' votes recorded (' + exact + ' rating)' );
-        }
- $('.ratings_stars').bind('click', function() {
-            var star = this;
-            var widget = $(this).parent();
-            
-            var clicked_data = {
-                clicked_on : $(star).attr('class'),
-                widget_id : widget.attr('id')
-            };
-            $.post(
-                'ratings.php',
-                clicked_data,
-                function(INFO) {
-                    widget.data( 'fsr', INFO );
-                    set_votes(widget);
-                },
-                'json'
-            ); 
-        });
+/**
+ * Initialize
+ */
+StarRating.prototype.init = function() {
+  this.stars = document.querySelectorAll('#rating span');
+  for (var i = 0; i < this.stars.length; i++) {
+    this.stars[i].setAttribute('data-count', i);
+    this.stars[i].addEventListener('mouseenter', this.enterStarListener.bind(this));
+  }
+  document.querySelector('#rating').addEventListener('mouseleave', this.leaveStarListener.bind(this));
+};
+
+/**
+ * This method is fired when a user hovers over a single star
+ * @param e
+ */
+StarRating.prototype.enterStarListener = function(e) {
+  this.fillStarsUpToElement(e.target);
+};
+
+/**
+ * This method is fired when the user leaves the #rating element, effectively removing all hover states.
+ */
+StarRating.prototype.leaveStarListener = function() {
+  this.fillStarsUpToElement(null);
+};
+
+/**
+ * Fill the star ratings up to a specific position.
+ * @param el
+ */
+StarRating.prototype.fillStarsUpToElement = function(el) {
+  // Remove all hover states:
+  for (var i = 0; i < this.stars.length; i++) {
+    if (el == null || this.stars[i].getAttribute('data-count') > el.getAttribute('data-count')) {
+      this.stars[i].classList.remove('hover');
+    } else {
+      this.stars[i].classList.add('hover');
+    }
+  }
+};
+
+// Run:
+new StarRating();
