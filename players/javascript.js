06ac9d8ce9cc262632cda3c614a4305e5feb8d75
eaajones <33253637+eaajones@users.noreply.github.com>
2017-12-12 10:15:38 +0100

    begyndende java

--- players/javascript.js
+++ players/javascript.js
@@ -1,7 +1,16 @@
 // JavaScript Document
 var playernumber=3;
-if (playernumber<=0) {
-	for (i = 0; i < 5; i++)
-	
-}
+//if (playernumber<=0) {
+	//for (i = 0; i < 5; i++)
+		
+//}
 
+var nummer3farve=0;
+var farvearray=('red','green','yellow','blue')
+
+var nummer3=setTimeout(function(){
+	$('#3').css('background-color:'+farvearray[nummer3farve]);
+	nummer3farve=nummer3farve+1;
+	
+	
+},1000)
