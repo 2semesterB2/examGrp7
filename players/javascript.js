a5c3a334d8de07f2925bdeb6833df76330c26ba8
Bror Strange Arnfast (lektor – bror@eaaa.dk) <bror@eaaa.dk>
2017-12-12 22:09:05 +0100

    Fixede testen af farve

--- players/javascript.js
+++ players/javascript.js
@@ -12,7 +12,7 @@ var score=0;
 var nummer3=setInterval(function(){
 	$('#3').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	}
-,2000);
+,1000);
 $('#3').click(function(){
 clearInterval(nummer3);
 antalklik=antalklik+1;
@@ -26,10 +26,12 @@ antalklik=antalklik+1;
 var nummer4=setInterval(function(){
 	$('#4').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);//https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
 	
-},2000);
+},1000);
 $('#4').click(function(){
 clearInterval(nummer4);
-	if($('#4').css('background-color')==='rgb(255,0,0)'){
+	if($('#4').css('background-color')=='rgb(255, 0, 0)'){
+//	if($('#4').css('background-color')=='rgb(255,0,0)'){      GAMMEL LINIE
+		//  vi havde ikke llagt mærke til at der var spaces efter kommaerne da vi kiggede på outputtet fra nummer 5
 		score=score+1;
 		
 		alert("re");
@@ -43,7 +45,7 @@ clearInterval(nummer4);
 var nummer5=setInterval(function(){
 	$('#5').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	
-},2000);
+},1000);
 $('#5').click(function(){
 clearInterval(nummer5);
 	alert($('#5').css('background-color'));
@@ -61,7 +63,7 @@ $('#5').off('click');
 var nummer6=setInterval(function(){
 $('#6').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	
-},2000);
+},1000);
 $('#6').click(function(){
 clearInterval(nummer6);
 	antalklik=antalklik+1;
