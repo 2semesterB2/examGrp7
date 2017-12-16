93f4705092b1d5d82db93fb195869c8015e4ba15
Hamzeh <hslipp@hotmail.com>
2017-12-16 16:48:40 +0100

    Leaderboard

---
+++ Leaderboard/leaderboard.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Rate Us</title>
+<link href="styletiles.css" rel="stylesheet" type="text/css">	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+<script src="javascript.js"></script>
+</head>
+
+	             <img id="color" src="image/color.jpg";>
+    <h1>Leaderboard list </h1>
+	
+<div id="leader">
+	
+<p id="green">    
+   Green Color:
+    </p>
+<p id="yellow">    
+   Yellow Color:
+    </p>
+<p id="blue">    
+   blue Color:
+    </p>
+<p id="red">    
+   Red Color:
+    </p>
+	</div>	
+	
+	
+<body>
+</body>
+</html>
