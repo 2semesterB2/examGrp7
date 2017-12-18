4c94177c74c25967eaedca2ed024d058485b93ec
Kaare123 <kaareandersen123@gmail.com>
2017-12-18 15:48:34 +0100

    hide

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <script src="Js/javascript.js"></script>
 </head>
 <body>
+<h1> Welcome to BaseGame. Play this game with your friends to find out, who is paying for the next round</h1>
 <a class="button" class="btn-link" href="players/players.php">Start</a>
 </body>
 </html>
