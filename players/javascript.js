b117e98f93b07b40b6ecd0deec289dfb11ba1fcd
eaajones <33253637+eaajones@users.noreply.github.com>
2017-12-18 12:55:55 +0100

    retret

--- players/javascript.js
+++ players/javascript.js
@@ -56,7 +56,7 @@ var score=0;
 var nummer3=setInterval(function(){
 	$('#3').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	}
-,1000);
+,350);
 $('#3').click(function(){
 clearInterval(nummer3);
 	if($('#3').css('background-color')=='rgb(0, 128, 0)'){
@@ -73,7 +73,7 @@ antalklik=antalklik+1;
 var nummer4=setInterval(function(){
 	$('#4').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);//https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
 	
-},1000);
+},350);
 $('#4').click(function(){
 clearInterval(nummer4);
 	if($('#4').css('background-color')=='rgb(0, 128, 0)'){
@@ -91,7 +91,7 @@ clearInterval(nummer4);
 var nummer5=setInterval(function(){
 	$('#5').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	
-},1000);
+},350);
 $('#5').click(function(){
 clearInterval(nummer5);
 	if($('#5').css('background-color')=='rgb(0, 128, 0)'){
@@ -112,7 +112,7 @@ $('#5').off('click');
 var nummer6=setInterval(function(){
 $('#6').css('background-color',farvearray[Math.floor(Math.random() * farvearray.length)]);
 	
-},1000);
+},350);
 $('#6').click(function(){
 clearInterval(nummer6);
 	if($('#6').css('background-color')=='rgb(0, 128, 0)'){
